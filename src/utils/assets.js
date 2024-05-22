158063f55036efb60e347797727e57e69966fee7
Tanmay Vaij <tanmayvaij22@gmail.com>
2024-05-22 14:48:22 +0530

    dir cleaned
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- src/utils/assets.js
+++
@@ -1,5 +0,0 @@
-export const redGradientButton = require('../../assets/svgs/red_button_gradient.svg');
-export const blueGradientButton = require('../../assets/svgs/blue_button_gradient.svg');
-export const leftGreyArrowIcon = require('../../assets/icons/atoms-icons-left-arrow-grey.png');
-export const loginBgImage = require('../../assets/images/login.jpg');
-export const closeImage = require('../../assets/icons/close.png');
